ee720c7d5a56d2d5a8454cf2383df2f3b843a949
Cindy Zacarias <cindyjzacarias@gmail.com>
2024-06-13 19:00:37 +0200

    Routes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../shared/nav/nav.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NavComponent, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+  userLoginOn:boolean=false;
 }
